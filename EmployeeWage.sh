634c2a3e54fafb2bddeafdda736bda54ff21d766
shashank srivastava <shashank.1696@gmail.com>
2020-07-16 21:01:43 +0530

    Employee'sWageForCondition

--- EmployeeWage.sh
+++ EmployeeWage.sh
@@ -4,18 +4,22 @@ wagePerHrs=20
 isFullTime=2
 isPartTime=1
 workDaysPerMonth=20
+workHrsPerMonth=0
 
-empPresent=$((RANDON%3))
+totalWorkingDays=0
+totalWorkingHrs=0
 
-case $empPresent in
-	
-	isFullTime) workingHrs=8;;
-	isPartTime) workingHrs=4;;
-		 *) workingHrs=0;;
-esac
+while [ $totalWorkingHrs -lt $workHrsPerMonth ] && [ $totalWorkingDays -lt $workDaysPerMonth ]
+do
+	((totalWorkingDays++))
+	empPresent=$((RANDOM%3))
+	case $empPresent in
+		$isFullTime) workingHrs=8;;
+		$isPartTime) workingHrs=4;;
+		          *) workingHrs=0;;
+	esac
+	totalWorkingHrs=$(( $totalWorkingHrs+$workingHrs ))
+done
 
-empDailyWage=$(( $wagePerHrs*$workingHrs ))
-echo "1day wage of a employee : " $empDailyWage 
-
-empMonthWage=$(( $empDailyWage*$workDaysPerMonth ))
-echo "1month wage of a employee : " $empMonthWage
+empTotalWage=$(( $totalWorkingHrs*$wagePerHrs ))
+echo "total wage of the employee : " $empTotalWage
