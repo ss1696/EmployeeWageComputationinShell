75ee4a0a7384359be0e2276a381dba441f96a7f0
shashank srivastava <shashank.1696@gmail.com>
2020-07-16 19:53:25 +0530

    Employee's DailyWage

--- EmployeeWage.sh
+++ EmployeeWage.sh
@@ -1,10 +1,17 @@
 #!/bin/bash
 
+wagePerHrs=20
+
 empPresent=$((RANDON%3))
 
 if [ $empPresent -eq 2 ]
 then
 	echo "employee is Present"
+	workingHrs=8
 else
 	echo "employee is Absent"
+	workingHrs=0
 fi
+
+empDailyWage=$(( $wagePerHrs*$workingHrs))
+echo "1 day wage of the employee : " $empDailyWage
